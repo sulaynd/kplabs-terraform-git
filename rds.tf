4f291777ea205e8a10ab14b7ae6bec3170958cf0
souleye <sulaynd@gmail.com>
2023-11-20 12:36:58 -0500

    RDS with tfstate

---
+++ rds.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 5
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "${file("../rds_pass.txt")}"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
